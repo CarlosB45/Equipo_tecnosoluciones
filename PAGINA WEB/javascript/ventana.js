d8f7766c416636c0e4057ba02a4bb4a3a1d7db5a
chrispincas <90738737+chrispincas@users.noreply.github.com>
2021-10-10 18:36:11 -0500

    Update ventana.js

--- PAGINA WEB/javascript/ventana.js
+++ PAGINA WEB/javascript/ventana.js
@@ -5,3 +5,12 @@ function abrir(){
 function cerrar(){
    document.getElementById('vent').style.display="none";
 }
+
+function recibir(){
+   var valor = document.getElementById("rol_sis").value;
+   if(valor != "" ){
+      alert("Se ha hecho un cambio en el rol del sistema");
+   }else{
+      alert("Ningun cambio en el rol del sistema");
+   }
+} 
