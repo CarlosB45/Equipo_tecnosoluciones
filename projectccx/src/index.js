073a30fec367e3aad0e71181d7581fc3b7c2615c
Ximena5745 <90114334+Ximena5745@users.noreply.github.com>
2021-10-17 11:21:26 -0500

    Add files via upload

--- projectccx/src/index.js
+++ projectccx/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import registerproduct from './mod_producto/Registerproduct';
 
 ReactDOM.render(
   <React.StrictMode>
