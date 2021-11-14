70e96f37a173bed18d8e4f1e88de68d3703e3dbb
chrispincas <christian.pinzon92@gmail.com>
2021-11-14 17:32:17 -0500

    Update index.js

--- projectccx/src/index.js
+++ projectccx/src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './/App';
-import reportWebVitals from '*/reportWebVitals';
-import { Auth0Provider } from "@auth0/auth0-react";
+import reportWebVitals from './reportWebVitals';
 //import ConlVend from './pages/mod_vendedor/index'
 
 ReactDOM.render(
