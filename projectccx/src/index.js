6f11e71f9e48952375ab2b86b6b5d0f1beb55f70
chrispincas <christian.pinzon92@gmail.com>
2021-11-14 17:00:38 -0500

    index

--- projectccx/src/index.js
+++ projectccx/src/index.js
@@ -8,9 +8,9 @@ import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
   <Auth0Provider
-  domain="development-web.us.auth0.com"
-  clientId="eSPZYvuh7RhZAXUwooRNaIXcx9INmou8"
-  redirectUri={window.location.origin}
+    domain="development-web.us.auth0.com"
+    clientId="eSPZYvuh7RhZAXUwooRNaIXcx9INmou8"
+    redirectUri={window.location.origin}
   >
     <App />
   </Auth0Provider>,
