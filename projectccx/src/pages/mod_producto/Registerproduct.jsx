b4acb2cb45fa93920dc5dabc1d5a9e8f66183861
Ximena5745 <90114334+Ximena5745@users.noreply.github.com>
2021-11-24 21:02:12 -0500

    Update Registerproduct.jsx

--- projectccx/src/pages/mod_producto/Registerproduct.jsx
+++ projectccx/src/pages/mod_producto/Registerproduct.jsx
@@ -1,18 +1,19 @@
 import React, {Component} from 'react';
-import "bootstrap/dist/css/bootstrap.min.css";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 import 'style/Styles.css';
 import images from 'images/index_img';
 import {Link} from 'react-router-dom';
 
-function Registerproduct () {
-    return(
-        <div className="registerproduct">
+const Registerproduct = ({ handleChange, handleClick, categorias, formValue }) => {
+    return (
+        <div className Registerproduct>
             <div className="sidebar">
                 <div className="sidebar-brand">
                     <h2><span className="Inventario"></span> <span></span></h2>
                 </div>
 
-                <div className="sidebar-menu">
+            <div className="sidebar-menu">
                     <ul>
                         <li>
                             <Link to ="Home" ><span className="home"><img src= {images.img_homew}/></span>
@@ -32,72 +33,132 @@ function Registerproduct () {
                         </li>
                     </ul>
                 </div>
-            </div>
+                </div>
+
+
+
             <header>
                 <div>
-                    <img src={images.img_logo} width="60px" height="60px"/>
+                <img src={images.img_logo} width="60px" height="60px"/>
                 </div>
                 <div className="name-company">
-                 <div>Tecnosoluciones</div>
+                    <div>Tecnosoluciones</div>
                 </div>
-                <div className="user-wrapper">
+        <       div className="user-wrapper">
                     <img src={images.img_user} width="40px" height="40px" alt=""/>
                     <p><h4>Administrador</h4>
                     <small>Super Admin</small></p>
                 </div>
             </header>
-            <main>
-                <div className= "Title1"> Registro de producto</div>
-                &nbsp;
-                <div className="field">
-                    <label className="labelform">Tipo de producto</label>
-                    <select className="formscontrol" required autocomplete>
-                        <option value= "">Seleccione</option>
-                        <option value= "C">Computador</option>
-                        <option value= "TV">Televisor</option>
-                        <option value= "W">SmartWatch</option>
-                        <option value= "J">Juegos</option>
-                    </select>
-                </div>
-                <div className="field">
-                    <label className="labelform">Código del producto</label>
-                    <input type="number" className="formscontrol" required autocomplete/>
-                </div>
-                <div className="field">
-                    <label className="labelform">Nombre del producto</label>
-                    <input type="text" className="formscontrol" required autocomplete/>
-                 </div>
-                <div className="field">
-                    <label className="labelform">Marca</label>
-                    <input type="text" className="formscontrol" required autocomplete/>
-                </div>
-                <div className="field">
-                    <label className="labelform">Referencia</label>
-                    <input type="text" className="formscontrol" required autocomplete/>
-                </div>
-                <div className="field">
-                    <label className="labelform">Valor unitario</label>
-                    <input type="text" className="formscontrol" required autocomplete/>
-                </div>
-                <div className="field">
-                    <label className="labelform">Proveedor</label>
-                <input type="text" className="formscontrol" required autocomplete/>
-                </div>
-                <div className="field">
-                    <label className="labelform">Imagen del producto</label>
-                    <input type="submit" className="boton" value="Cargar Imagen"/>
-                </div>
-                <div className="field">
-                    <label className="labelform">Almacenamiento</label>
-                    <input type="text" className="formscontrol" required autocomplete/>
-                </div>
-                <div className="field">
-                    <input type="submit" className="boton" value="Guardar Producto"/>
-                </div>
-            </main>
-        </div>
 
+                <main>
+                <div className= "Title1"> Registro de producto</div>
+                <div className = "main-content">
+                    
+                <Form>      
+
+                <Form.Select aria-label="Default select example">
+                    <option>Seleccione una categoria</option>
+                    <option value="TV">Televisor</option>
+                    <option value="C">Computador</option>
+                    <option value="W">SmartWatch</option>
+                    <option value="J">Juegos</option>
+                </Form.Select>
+                
+                    <Form.Group className="mb-3">
+                        <Form.Label>Código del producto</Form.Label>
+                        <Form.Control
+                        type="text"
+                        name="codigo"
+                        onChange={handleChange}
+                        />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3">
+                        <Form.Label>Nombre del Producto</Form.Label>
+                        <Form.Control
+                         type="text"
+                        name="nombre"
+                        style={{ height: "50px" }}
+                        onChange={handleChange}
+                        
+                        />
+                    </Form.Group>
+                    
+                    <Form.Group className="mb-3">
+                        <Form.Label>Especificaciones</Form.Label>
+                        <Form.Control
+                        as="textarea"
+                        name="especificaciones"
+                        onChange={handleChange}
+                        />
+                    </Form.Group>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Referencia</Form.Label>
+                        <Form.Control
+                        type="text"
+                        name="especificaciones"
+                        onChange={handleChange}
+                        />
+                    </Form.Group>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Marca</Form.Label>
+                        <Form.Control
+                        type="text"
+                        name="especificaciones"
+                        onChange={handleChange}
+                        />
+                    </Form.Group>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Valor Unitario</Form.Label>
+                        <Form.Control
+                        type="text"
+                        name="valorunit"
+                        onChange={handleChange}
+                        />
+                    </Form.Group>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Proveedor</Form.Label>
+                        <Form.Control
+                        type="text"
+                        name="proveedor"
+                        onChange={handleChange}
+                        />
+                    </Form.Group>
+
+                    <Form.Group controlId="formFileDisabled" className="mb-3">
+                        <Form.Label>Cargar imagen del producto</Form.Label>
+                        <Form.Control type="file"
+                        name="imagen" 
+                        onChange= {handleChange}/>
+                        </Form.Group>
+
+                    <Form.Group className="mb-3">
+                        <Form.Label>Almacenamiento</Form.Label>
+                        <Form.Control
+                        type="text"
+                        name="almacenamiento"
+                        onChange={handleChange}
+                        />
+                    </Form.Group>
+
+                    <Button type="button" variant="danger">
+                    Cancelar
+                    </Button>
+                    <Button  
+                    onClick={handleClick}
+                    type="button"
+                    variant="danger"
+                    className="float-end"
+                        >
+                    Guardar
+                    </Button>
+
+                </Form>
+                </div>
+                </main>
+                </div>
 
     );
-}
+};
     export default Registerproduct;
